df1b13f1e68997a90daf67e09741b8a35729d7df
LCHARLOPEAU <luc.charlopeau@ynov.com>
2022-03-31 13:17:07 +0200

    mutability

---
+++ mutability.js
@@ -0,0 +1,18 @@
+const clone1 = {
+    name: person.name,
+    age: person.age+1,
+    country: 'FR'
+ };
+ Object.freeze(clone1);
+ const clone2 = {
+    name: person.name,
+    age: person.age+1,
+    country: 'FR'
+ };
+ Object.freeze(clone2);
+ const samePerson = {
+    name: person.name,
+    age: person.age,
+    country: person.country
+ };
+  
