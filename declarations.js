53c8616797d7327763a03b469a75af828e7a2fef
LCHARLOPEAU <luc.charlopeau@ynov.com>
2022-03-31 12:49:25 +0200

    primitives

--- declarations.js
+++ declarations.js
@@ -3,6 +3,9 @@ const arr = [4, '2'];
 const obj = {str: "string",num: 5,bool: true,undef: undefined}; 
 const nested = {
     arr:[4,undefined,'2'],
-    obj
- }
- console.log(escapeStr);
+    obj: {
+       str:"limousine",
+       num:5,
+       bool:true 
+    }
+ };
