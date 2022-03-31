a51845348e38366cd65565e558853a5ce38a7143
LCHARLOPEAU <luc.charlopeau@ynov.com>
2022-03-31 13:02:29 +0200

    primitives

--- declarations.js
+++ declarations.js
@@ -4,9 +4,8 @@ const obj = {str: "string",num: 5,bool: true,undef: undefined};
 const nested = {
     arr:[4,undefined,'2'],
     obj: {
-       str:"limousine",
-       num:5,
-       bool:true
+        str: "stringe",
+        num: 15,
+        bool: false,
     }
  };
-Object.freeze(obj);
