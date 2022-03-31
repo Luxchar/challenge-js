63aec7d8d343edc3b02f25e2c0ec4aad5db2e286
Luxchar <luccharlopeau@gmail.com>
2022-03-31 21:43:54 +0200

    abs

---
+++ abs.js
@@ -0,0 +1,13 @@
+function isPositive(nb) {
+    if (nb >= 0) {
+        return true;
+    }
+    return false;
+}
+
+function abs(nb) {
+    if (nb >= 0) {
+        return nb;
+    }
+    return -nb;
+}
