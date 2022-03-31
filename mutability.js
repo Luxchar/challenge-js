01249736fe04c2cb90432493e8c1d8676f371956
LCHARLOPEAU <luc.charlopeau@ynov.com>
2022-03-31 15:27:20 +0200

    mutability

--- mutability.js
+++ mutability.js
@@ -1,8 +1,6 @@
 person.age+=1
 person.country='FR'
-const clone1 = person
-Object.freeze(clone1);
-const clone2 = person
-Object.freeze(clone2);
+const clone1 = Object.assign({},person)
+const clone2 = Object.assign({},person)
 const samePerson = person
   
