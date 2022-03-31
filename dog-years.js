3da26d4fc9677f345e8502fa2c2da23f7d016506
Luxchar <luccharlopeau@gmail.com>
2022-03-31 21:16:34 +0200

    dogyears

--- dog-years.js
+++ dog-years.js
@@ -1,24 +1,18 @@
-function dogYears(planet,age) {
-    if (planet === 'mercury') {
-        return age*0.2408467;
-    }
-    if (planet === 'venus') {
-        return age*0.61519726;
-    }
-    if (planet === 'mars') {
-        return age*1.8808158;
-    }
-    if (planet === 'jupiter') {
-        return age*11.862615;
-    }
-    if (planet === 'saturn') {
-        return age*29.447498;
-    }
-    if (planet === 'uranus') {
-        return age*84.016846;
-    }
-    if (planet === 'neptune') {
-        return age*164.79132;
-    }
-    return age;
-  }
+let yearinsec = 31557600; //time of a year in seconds on earth
+const planets = {
+    earth: 1,
+    mercury: 0.2408467,
+    venus: 0.61519726,
+    mars: 1.8808158,
+    jupiter: 11.862615,
+    saturn: 29.447498,
+    uranus: 84.016846,
+    neptune: 164.79132,
+}
+function dogYears(planet, dogsAge) {
+    dogsAge /= yearinsec
+    let orbitalperiod = planets[planet]
+    dogsAge = dogsAge / orbitalperiod * 7 //convert to dog years (*7) for specific planet (period)
+    return Math.round(dogsAge * 100) / 100; //round to 2 decimal places
+}
+console.log(dogYears('earth', 1000000000))
