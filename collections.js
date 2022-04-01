f29c25a66ecbf9654d497cfe84c9ea331a508543
LCHARLOPEAU <luc.charlopeau@ynov.com>
2022-04-01 10:33:38 +0200

    marredeslicer

---
+++ collections.js
@@ -0,0 +1,5 @@
+let arrtoset = (arg) => new Set(arg);
+let arrtostr = (arg) => arg.join('');
+let settoarr = (arg) => arg.from(Set);
+let settostr = (arg) => Array.from(arg).join(' ');
+let strtoarr = (arg) => str.split("");
--- slicer.js
+++ slicer.js
@@ -1,23 +1,25 @@
 function slice(input, start = 0, end = input.length) {
     let res = ''
     let arr = []
-    if (start < 0) {
+    if (start < 0) { //negative handling
         start = -start
         start = input.length - start
     }
-    if (end < 0) {
+    if (end < 0) { //negative handling
         end = -end
         end = input.length - end
     }
     for (let i = start; i <= end; i++) {
+        if (i >= start && i < end) {
             if (typeof input == 'string') {
                 res += input[i]
             } else if (Array.isArray(input)) {
                 arr.push(input[i])
             }
+        }
     }
-    if (typeof input == 'string') {
+    if (typeof input == 'string') {//str
         return res
     }
-    return arr 
+    return arr //arr
 }
