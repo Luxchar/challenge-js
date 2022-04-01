99a20fcab2829d06afbf203435ced2ab5c1a04f2
LCHARLOPEAU <luc.charlopeau@ynov.com>
2022-04-01 09:31:46 +0200

    slicer

---
+++ slicer.js
@@ -0,0 +1,17 @@
+function slicer(array, start = 0, end = array.length) {
+    let res = ''
+    let arr = []
+    for (let i = 0; i <= arr.length - 1; i++) {
+        if (i >= start && i < end) {
+            if (typeof arr == 'string') {
+                res += arr[i]
+            } else if (Array.isArray(arr)) {
+                arr.push(arr[i])
+            }
+        }
+    }
+    if (typeof arr == 'string') {
+        return res
+    }
+    return arr 
+}
