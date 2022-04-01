4802bcac495c6a3516f04bffb4adad51b53e595b
Luxchar <luccharlopeau@gmail.com>
2022-04-01 23:51:43 +0200

    pyramid

--- pyramid.js
+++ pyramid.js
@@ -7,6 +7,9 @@ function pyramid(str, nb) {
         for (let k = 0; k < 2 * i - 1; k++) { // printing star
           res += str;
         }
+        if (i == nb) {
+            return res
+        }
         res += "\n";
       }
     return res
