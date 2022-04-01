c4534f10265b635b2a9296c465733f2efca02785
LCHARLOPEAU <luc.charlopeau@ynov.com>
2022-04-01 14:38:25 +0200

    collections

--- collections.js
+++ collections.js
@@ -1,7 +1,7 @@
 let arrToSet = (arg) => new Set(arg);
 let arrToStr = (arg) => arg.join('');
 let setToArr = (arg) => arg.from(Set);
-let setToStr = (arg) => Array.from(arg).join(' ');
+let setToStr = (arg) => arrToStr(Array.from(arg));
 let strToArr = (arg) => arg.split("");
 let strToSet = (arg) => [...arg].join(' ');
 let mapToObj = (arg) => Object.fromEntries(arg);
