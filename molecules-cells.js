063c4031fa2392eebb69b5a9f2d7fe8a32d93d3c
Luxchar <luccharlopeau@gmail.com>
2022-04-01 23:29:42 +0200

    molecules

---
+++ molecules-cells.js
@@ -0,0 +1,37 @@
+function DNA(RNA) {
+    let res = ''
+    for (let i = 0; i < RNA.length; i++) {
+        if (RNA[i] === 'C') {
+            res += 'G'
+        }
+        if (RNA[i] === 'G') {
+            res += 'C'
+        }
+        if (RNA[i] === 'A') {
+            res += 'T'
+        }
+        if (RNA[i] === 'U') {
+            res += 'A'
+        }
+    }
+    return res
+}
+
+function RNA(DNA) {
+    let res = ''
+    for (let i = 0; i < DNA.length; i++) {
+        if (DNA[i] === 'G') {
+            res += 'C'
+        }
+        if (DNA[i] === 'C') {
+            res += 'G'
+        }
+        if (DNA[i] === 'T') {
+            res += 'A'
+        }
+        if (DNA[i] === 'A') {
+            res += 'U'
+        }
+    }
+    return res
+}
