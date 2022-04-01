4292bb000454d8b9368955581dd70e46d7b12167
LCHARLOPEAU <luc.charlopeau@ynov.com>
2022-04-01 09:41:31 +0200

    slicer

--- slicer.js
+++ slicer.js
@@ -1,4 +1,4 @@
-function slicer(array, start = 0, end = array.length) {
+function slice(array, start = 0, end = array.length) {
     let res = ''
     let arr = []
     for (let i = 0; i <= arr.length - 1; i++) {
