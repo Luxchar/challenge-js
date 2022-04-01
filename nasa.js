516144da9001dd42e5959c48c44e1dc0cd000bf3
Luxchar <luccharlopeau@gmail.com>
2022-04-01 23:18:52 +0200

    nasa

---
+++ nasa.js
@@ -0,0 +1,16 @@
+function nasa(n) {
+    res = "" 
+    for (let i = 0; i < n; i++) {
+        if (i % 3 === 0) {
+            res += 'NA '
+        } else if (i % 5 === 0) {
+            res += 'SA '
+        } else if (i % 5 === 0 && i % 3 === 0) {
+            res += 'NASA '
+        } else {
+            res += i+' '
+        }
+    }
+    res = res.slice(0, res.length - 1)
+    return res
+}
