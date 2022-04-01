62cc044daf1f7eb1d65667e5750185abfd8f4b8c
Luxchar <luccharlopeau@gmail.com>
2022-04-01 23:41:22 +0200

    triangle

--- triangle.js
+++ triangle.js
@@ -1,6 +1,6 @@
 function triangle(str, nb) {
     let res = ""
-    for (let i = 1; i < nb; i++) {
+    for (let i = 1; i <= nb; i++) {
         for (let j = 0; j < i; j++) {
             res+=str
         }
