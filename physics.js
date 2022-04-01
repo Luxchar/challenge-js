4494ff5df4fcb4f82e3b382220a2866c773cea8b
LCHARLOPEAU <luc.charlopeau@ynov.com>
2022-04-01 10:13:15 +0200

    physics

--- physics.js
+++ physics.js
@@ -1,11 +1,9 @@
 function getAcceleration(object) {
     if (!Number.isNaN(object.f/object.m)) {
         return object.f/object.m;
-    }
-    if (!Number.isNaN(object.Δv/object.Δt)) {
+    } else if (!Number.isNaN(object.Δv/object.Δt)) {
         return object.Δv/object.Δt;
-    }
-    if (!Number.isNaN((object.d*2)/object.t**2)) {
+    } else if (!Number.isNaN((object.d*2)/object.t**2)) {
         return object.force/object.mass;
     }
     return 'impossible';
