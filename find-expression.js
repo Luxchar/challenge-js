b62cd9e64da79c3f4c88c73d0e5f45244f1ef6b7
Luxchar <luccharlopeau@gmail.com>
2022-04-03 14:38:31 +0200

    findexpression

--- find-expression.js
+++ find-expression.js
@@ -1,6 +1,6 @@
-function findExpression(nb, res = 1, seq ='') {
+function findExpression(nb, res = 1, seq = "") {
     if (res === nb) {
-        return '1 '+seq;
+        return '1 ' + seq.substring(0, seq.length - 1)
     }
     if (res < nb) {
         return findExpression(nb, res*=parseInt(mul2[1]), seq+=mul2+' ') || findExpression(nb, res+=parseInt(add4[1]), seq+=add4+' ') || undefined ;
