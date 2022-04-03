4247ea6e9c50c47aa25988c50114b8f8a3eb944c
Luxchar <luccharlopeau@gmail.com>
2022-04-03 12:07:43 +0200

    flat

--- flat.js
+++ flat.js
@@ -2,8 +2,8 @@ function flat(items, depth = 1) {
     const arr = [];
   
     items.forEach(item => {
-      if (Array.isArray(item)) {
-        arr.push(...flat(item, depth--));
+      if (Array.isArray(item) && depth > 0) {
+        arr.push(...flat(item, depth - 1));
       } else {
         arr.push(item);
       }
