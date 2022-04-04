e1e1ca85ac763d0771b622f626f6847cee416943
Luxchar <luccharlopeau@gmail.com>
2022-04-04 16:36:17 +0200

    letterspacenumber

---
+++ letter-space-number.js
@@ -0,0 +1,9 @@
+function letterSpaceNumber(str) {
+    let res = [];
+    for (let i = 0; i < str.length; i++) {
+        if (str[i].match(/[a-z]/i) && str[i+1] === ' ' && str[i+2].match(/[0-9]/) && !str[i+3].match(/[0-9]/)){
+            res.push(str[i]);
+        }
+    }
+    return res;
+}
