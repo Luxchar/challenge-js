22dc6f35b0215cd07238ea55c24a3e578422cd0b
Luxchar <luccharlopeau@gmail.com>
2022-04-04 20:15:33 +0200

    same-amount.js

--- same-amount.js
+++ same-amount.js
@@ -1,6 +1,6 @@
 function sameAmount(data,reg1,reg2) {
-    const r1 = new RegExp(reg1,'z')
-    const r2 = new RegExp(reg2,'z')
+    const r1 = new RegExp(reg1,'g') //g for every occurence (flag)
+    const r2 = new RegExp(reg2,'g')
     // console.log(regex1,'-',regex2)
     const resr1 = data.match(r1)
     const resr2 = data.match(r2)
