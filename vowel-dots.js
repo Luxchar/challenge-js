c72d6a1893d75f3399f5a39879db47782da7a580
Luxchar <luccharlopeau@gmail.com>
2022-04-04 18:09:38 +0200

    vowel-dots.js

---
+++ vowel-dots.js
@@ -0,0 +1,16 @@
+function vowelDots(str) {
+    for (let i = 0; i < str.length; i++) {
+        if (isVowelRegEx(str[i])) {
+            if (str[i+1] === ".") {
+                str = str.substring(0, i) + str.substring(i+1)
+                i = i-1
+            }
+        }
+    }
+}
+
+function isVowelRegEx(char) {
+    if (char.length == 1) {
+      return /[aeiou]/.test(char);
+    }
+  }
