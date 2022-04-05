5a3c3d58ef4cb1fb8681dc168e885b570c1e19d6
Luxchar <luccharlopeau@gmail.com>
2022-04-05 19:16:53 +0200

    drstrange

---
+++ dr-strange.js
@@ -0,0 +1,16 @@
+function addWeek(date) {
+    let day01 = new Date('0001-01-01')
+    let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    let diff = (((date-day01) / (60 * 60 * 24 * 1000))/7) %2 //diff is the number of weeks
+    if(diff%2===0){
+        return days[date.getDay()];
+    }
+    else {
+        return 'second'+days[date.getDay()];
+    }
+}
+
+function timeTravel({date,hour,minute,second}){
+    date.setHours(hour),date.setMinutes(minute),date.setSeconds(second) //functions built in for changing date
+    return date
+ }
