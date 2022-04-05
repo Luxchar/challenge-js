b3ca98472b5023e1d9e9862b4e5f5d9cba73d040
Luxchar <luccharlopeau@gmail.com>
2022-04-05 14:18:32 +0200

    Âgreedy

--- greedy-url.js
+++ greedy-url.js
@@ -1,5 +1,5 @@
 //getURL that returns an array with all valid URLs present in a data-set, http and https
-function getURL() {
+function getURL(data) {
     var url = [];
     var urlRegex = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/gi;
     var urlArray = data.match(urlRegex);
