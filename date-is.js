cc3decbb9a48ca69205313c566ccef98d1257a58
Luxchar <luccharlopeau@gmail.com>
2022-04-05 19:29:34 +0200

    dateis

---
+++ date-is.js
@@ -0,0 +1,37 @@
+function isValid(date) {
+    if (isNaN(date)) {
+        return false
+    }
+    return true
+}
+
+function isAfter(date01,date02) {
+    if(date01.getTime() > date02.getTime())
+    {
+        return true
+    }
+    return false
+}
+
+function isBefore(date01,date02) {
+    if(date01.getTime() < date02.getTime())
+    {
+        return true
+    }
+    return false
+}
+
+function isFuture(date) {
+    if(date.getTime() > Date.now())
+    {
+        return true
+    }
+    return false
+}
+function isPast(date) {
+    if(date.getTime() < Date.now())
+    {
+        return true
+    }
+    return false
+}
