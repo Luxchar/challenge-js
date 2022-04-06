0b85874234c50c7b8cfa6a26cef46347d629a463
Luxchar <luccharlopeau@gmail.com>
2022-04-06 17:39:52 +0200

    day-of-the-year.js

--- day-of-the-year.js
+++ day-of-the-year.js
@@ -1,5 +1,7 @@
-function dayOfTheYear(now) {
+function dayOfTheYear(date) {
+    var now = new Date(date.getFullYear(),date.getMonth(),date.getDay());
     var start = new Date(now.getFullYear(), 0, 0);
+    console.log(now,start)
     var diff = now - start;
     var oneDay = 1000 * 60 * 60 * 24;
     var day = Math.floor(diff / oneDay);
