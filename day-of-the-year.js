52a567ece85774a3c499e9ebbad442b5abb370bc
LCHARLOPEAU <luc.charlopeau@ynov.com>
2022-04-07 10:26:11 +0200

    day-of-the-year.js

--- day-of-the-year.js
+++ day-of-the-year.js
@@ -1,8 +1,11 @@
 function dayOfTheYear(date) {
     var now = new Date(date.getFullYear(),date.getMonth(),date.getDay());
-    var start = new Date(date.getFullYear(),0,1);
+    var start = new Date(date.getFullYear(),0,0);
     var diff = now-start;
-    var oneDay = 1000 * 60 * 60 * 24;
+    var oneDay = 1000 * 3600 * 24;
     var day = Math.floor(diff / oneDay);
     return day
 }
+
+// console.log(dayOfTheYear(new Date('1664-08-09')))
+// console.log(dayOfTheYear(new Date('0001-01-01')))
