4e88a17775ae365d024988dbe74c29decebaf34f
LCHARLOPEAU <luc.charlopeau@ynov.com>
2022-04-07 11:24:38 +0200

    bloody-sunday.js

---
+++ bloody-sunday.js
@@ -0,0 +1,13 @@
+function bloodySunday(date) {
+    var now = new Date('0001-01-01');
+    var end = new Date(date.getFullYear(), date.getMonth(), date.getDate());
+    let days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    for(let i = 0; i < 6 ; i++){ 
+        let today = days[now.getDay()-1];
+        if(now.getDate() === end.getDate()){
+            return today
+        }
+        now.setHours(-24)    
+    }
+    return time 
+}
--- day-of-the-year.js
+++ day-of-the-year.js
@@ -1,7 +1,6 @@
 function dayOfTheYear(date) {
     var now = new Date(date.getFullYear(), date.getMonth(), date.getDate());
     var start = new Date(date.getFullYear(),0,0);
-    console.log(start,now)
     var diff = now-start;
     var oneDay = 1000 * 3600 * 24;
     var day = Math.floor(diff / oneDay);
