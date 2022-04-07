11a0c524fc08737015bbe35db65f8c7d8d526b00
LCHARLOPEAU <luc.charlopeau@ynov.com>
2022-04-07 11:29:05 +0200

    bloody-sunday.js

--- bloody-sunday.js
+++ bloody-sunday.js
@@ -1,10 +1,9 @@
 function bloodySunday(date) {
-    var now = new Date('0001-01-01');
+    var now = new Date(date.getFullYear(), date.getMonth(), 0);
     var end = new Date(date.getFullYear(), date.getMonth(), date.getDate());
-    let hours = week*7*24 //how many hours have passed
-    time.setHours(hours-126) //dont know why but there's a spare week 
-    
+
     let days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    let today = days[end.getDay()];
     for(let i = 0; i < 6 ; i++){ 
         let today = days[now.getDay()-1];
         if(now.getDate() === end.getDate()){
