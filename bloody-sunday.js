389f1ef31d907a894168330c8b9fdeca1ef188ba
LCHARLOPEAU <luc.charlopeau@ynov.com>
2022-04-07 14:41:02 +0200

    bloody-sunday.js

--- bloody-sunday.js
+++ bloody-sunday.js
@@ -1,25 +1,19 @@
 function bloodySunday(date) {
     var now = new Date(date.getFullYear(), date.getMonth(), 0);
-    var end = new Date(date.getFullYear(), date.getMonth(), date.getDate());
+    var end = new Date(date.getFullYear(), date.getMonth(), date.getDay());
 
     let days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     var count = 0
     for(let i = 0; i < 31 ; i++){ 
-        
-        if(now.getDay() === end.getDay()){
-            return days[6-count]
+        if (count > 5) {
+            count = 0
+        }
+        if(now.getDate() === end.getDate()){
+            console.log(count)
+            return days[count]
         }
 
+        count++
         now.setHours(-24)
-        if (count === 6) {
-            count = 0
-        } else {
-            count++;  
-        } 
     }
 }
-
-// console.log(bloodySunday(new Date('0001-01-01')))
-// console.log(bloodySunday(new Date('0001-01-06')))
-// console.log(bloodySunday(new Date('0001-01-07')))
-// console.log(bloodySunday(new Date('0001-12-01')))
