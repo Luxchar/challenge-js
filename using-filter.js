63c0b8f0985ad2ea686776b8118e1ba482c7de57
Luxchar <luccharlopeau@gmail.com>
2022-04-10 13:00:35 +0200

    using-filter.js

---
+++ using-filter.js
@@ -0,0 +1,32 @@
+function filterShortStateName(arr) {
+    return arr.filter(function(state) {
+        return state.length === 7;
+    });
+}
+
+function filterStartVowel(arr) {
+    return arr.filter(function(state) {
+        return state[0].match(/[aeiou]/i);
+    });
+}
+
+function filter5Vowels(arr) {
+    return arr.filter(function(state) {
+        return state.match(/[aeiou]{5}/i);
+    });
+}
+
+function filter1DistinctVowel(arr) {
+    return arr.filter(function(state) {
+        return state.match(/[aeiou]{1}/i);
+    });
+}
+
+function multiFilter(objects) {
+    return objects.filter(function(object) {
+        return object.capital.length >= 8 &&
+            !object.name[0].match(/[aeiou]/i) &&
+            object.tag.match(/[aeiou]/i) &&
+            object.region !== 'South';
+    });
+}
