d8352c6b1f2fd892cb5ed3c66fa840a9abfa4eef
LCHARLOPEAU <luc.charlopeau@ynov.com>
2022-04-11 11:14:38 +0200

    filter.js

--- filter.js
+++ filter.js
@@ -22,13 +22,12 @@ function reject(arr, func) {
 
 //Create a partition function that takes an array as first argument, a function as second, and that works like the partition function from lodash.
 function partition(arr, func) {
-    var newArr01 = [];
-    var newArr02 = [];
+    var newArr01,newArr02  = []
     for (var i = 0; i < arr.length; i++) {
         if (func(arr[i],i,arr)) {
-            newArr01[0].push(arr[i]);
+            newArr01.push(arr[i]);
         } else {
-            newArr02[1].push(arr[i]);
+            newArr02.push(arr[i]);
         }
     }
     return newArr01+newArr02;
