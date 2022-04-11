09f1cefb45c8d6df175bfa409bd8891e03bc7bfa
LCHARLOPEAU <luc.charlopeau@ynov.com>
2022-04-11 10:09:40 +0200

    for-each.js

--- for-each.js
+++ for-each.js
@@ -1,6 +1,8 @@
 //Create a forEach function that takes an array as first argument, a function as second, and that works like the method .forEach
 function forEach(arr, func) {
+    let res = []
     for (var i = 0; i < arr.length; i++) {
-        func(arr[i]);
+        res.push(func(arr[i],i,arr));
     }
+    return res
 }
