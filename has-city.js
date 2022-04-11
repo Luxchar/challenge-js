49d093ff9d69d2034d205f6ae9e20ad967697fc1
LCHARLOPEAU <luc.charlopeau@ynov.com>
2022-04-11 12:03:38 +0200

    has-city.js

---
+++ has-city.js
@@ -0,0 +1,14 @@
+// Create a function hasCity that takes a string called country and an array of strings, which are cities of that country, which returns a new function.
+// The new function takes a string, called city, as a parameter and which determines whether the array contains that string or not.
+
+// If it does, it should return the string <city> is a city from <country>, otherwise <city> is not a city from <country>.
+
+function hasCity(country, cities) {
+    return function(city) {
+        if (cities.indexOf(city) > -1) {
+            return city + " is a city from " + country;
+        } else {
+            return city + " is not a city from " + country;
+        }
+    }
+}
