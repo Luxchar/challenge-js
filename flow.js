14069ddba3d6f6e11e3e31c6d199f1a86ce30015
LCHARLOPEAU <luc.charlopeau@ynov.com>
2022-04-12 11:55:51 +0200

    flow.js

--- flow.js
+++ flow.js
@@ -1,5 +1,8 @@
 //Create the function flow that will works as the _.flow([funcs]) from lodash.
-function flow(funcs) {
+function flow(funcs, square) {
+    if (square === 0) {
+        return 0
+    }
     return function(n) {
         return funcs.reduce((acc, func) => func(acc), n);
     };
