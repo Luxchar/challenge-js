b9cddbc07e6cf617371555c64a5dcb218b50aa28
Luxchar <luccharlopeau@gmail.com>
2022-04-15 15:43:33 +0200

    pick-omit.js

--- pick-omit.js
+++ pick-omit.js
@@ -1,7 +1,3 @@
-// Create a pick function that takes an object and a string or array of strings. This function returns a new object that contains only the key/value pairs, whose key matches with the string or a string in the array, from the object passed as an argument.
-
-// Create a omit function that takes an object and a string or array of strings. This function returns a new object that contains only the key/value pairs, whose key doesn't match with the string or a single string in the array, from the object passed as an argument.
-
 function pick(obj, data) {
     let res = {}
     let keys = Object.keys(obj)
