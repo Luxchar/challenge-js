867334f35ddee96bc4b77381039d71f988e1bf28
Luxchar <luccharlopeau@gmail.com>
2022-04-15 13:01:35 +0200

    keycodes-symphony.js

---
+++ keycodes-symphony.js
@@ -0,0 +1,21 @@
+export function compose(){
+    document.addEventListener("keydown", (event)=>{
+        let event = event.keyCode //needs keycode for color change
+        if(event === 27){ //esc key
+            document.querySelectorAll("div").forEach((item) => {
+                item.remove()
+            })
+        }else if(event === 8){ //backspace key
+            let removeletter = document.querySelectorAll("div")
+            removeletter[removeletter.length-1].remove()
+        }else{
+            let eventletter = event.key
+            let block = document.createElement("div")
+            block.classList.add("note")
+            let color = "#"+99+event+event
+            block.style.background = color
+            document.body.appendChild(block)
+            block.innerHTML = eventletter
+        }
+    })
+}
