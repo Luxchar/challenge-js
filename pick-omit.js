1530daa91a74496a5babdf4bf460e640b47341c4
Luxchar <luccharlopeau@gmail.com>
2022-04-15 15:49:51 +0200

    pick-omit.js

--- pick-omit.js
+++ pick-omit.js
@@ -2,6 +2,11 @@ function pick(obj, data) {
     let res = {}
     let keys = Object.keys(obj)
     keys.forEach(key => {
+        if (Array.isArray(data)) {
+            if (data.includes(key)) {
+                res[key] = obj[key]
+            }
+        }
         if (key == data) {
             res[key] = obj[key]
         }
@@ -13,6 +18,11 @@ function omit(obj, data) {
     let res = {}
     let keys = Object.keys(obj)
     keys.forEach(key => {
+        if (Array.isArray(data)) {
+            if (!data.includes(key)) {
+                res[key] = obj[key]
+            }
+        }
         if (key != data) {
             res[key] = obj[key]
         }
