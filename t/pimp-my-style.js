859737f3cd37554ce2420cdf0991fb81cf7cb287
Luxchar <luccharlopeau@gmail.com>
2022-04-15 11:51:38 +0200

    pimp-my-style.js

--- t/pimp-my-style.js
+++ t/pimp-my-style.js
@@ -32,18 +32,19 @@ let pimper = 0
 let unpimp = 15
 export function pimp(){
     let button = document.querySelector("button")
-    if (pimper >= styles.length){
+    if (pimper >= 15){
         button.classList.remove(styles[unpimp])
         unpimp--
         if (unpimp === -1) {
+            button.classList.remove('unpimp')
             pimper = 0
         }
     } else {
         button.classList.add(styles[pimper])
         pimper++
-        if (pimper > styles.length) {
+        if (pimper > 15) {
             button.classList.add('unpimp')
-            unpimp = styles.length
+            unpimp = 15
         }
     }
 }
