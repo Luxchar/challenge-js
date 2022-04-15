89836d4ebb7f5967063910ba7d4617c7b1ffd750
Luxchar <luccharlopeau@gmail.com>
2022-04-15 15:10:03 +0200

    invert.js

--- invert.js
+++ invert.js
@@ -1,9 +1,11 @@
 // Create a function called invert that takes an object and returns it with its keys and values inverted.
-
+  // It should ignore properties from the prototype chain
 function invert(obj) {
-  var newObj = {};
-  for (var key in obj) {
-    newObj[obj[key]] = key;
-  }
-  return newObj;
+    var newObj = {};
+    for (var key in obj) {
+        if (obj.hasOwnProperty(key)) {
+        newObj[obj[key]] = key;
+        }
+    }
+    return newObj;
 }
