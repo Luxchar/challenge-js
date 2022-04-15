94b351e29d4bf7f04cff0bd79993182a88397bcc
Luxchar <luccharlopeau@gmail.com>
2022-04-15 11:00:42 +0200

    pimp-my-style.js

---
+++ pimp-my-style.js
@@ -0,0 +1,23 @@
+import {styles} from './data.js'
+let pimper = 0
+let unpimp = 14
+export function pimp(){
+    let elem = document.querySelector("button")
+    if (pimper < styles.length){
+        unpimp = 14
+        elem.classList.add(styles[k])
+        pimper++
+        if(pimper == 15){
+            elem.classList.add('unpimp')
+        }
+    } else {
+        elem.classList.add('unpimp')
+        elem.classList.remove(styles[unpimp])
+        pimper++
+        unpimp--
+        if(unpimp == -1){
+            elem.classList.remove('unpimp')
+        }
+            
+     }
+}
