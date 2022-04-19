f3a9d50e43e62613d9f65e5ff2cf0828362f57a5
Luxchar <luccharlopeau@gmail.com>
2022-04-19 12:10:53 +0200

    manipulate-keys.js

--- manipulate-keys.js
+++ manipulate-keys.js
@@ -42,10 +42,21 @@ function mapKeys(cart, callback) {
 function reduceKeys(cart, callback) {
     let newCart = '';
     for (let key in cart) {
-        newCart += callback(key,'');
+        newCart = callback(newCart,key);
     }
-    if (newCart[newCart.length-1] !== ' ') {
-        return ":"+newCart
-    }
-    return newCart.substring(0, newCart.length - 2);     
+    return newCart;   
 }
+// const nutritionDB = {
+//     tomato:  { calories: 18,  protein: 0.9,   carbs: 3.9,   sugar: 2.6, fiber: 1.2, fat: 0.2   },
+//     vinegar: { calories: 20,  protein: 0.04,  carbs: 0.6,   sugar: 0.4, fiber: 0,   fat: 0     },
+//     oil:     { calories: 48,  protein: 0,     carbs: 0,     sugar: 123, fiber: 0,   fat: 151   },
+//     onion:   { calories: 0,   protein: 1,     carbs: 9,     sugar: 0,   fiber: 0,   fat: 0     },
+//     garlic:  { calories: 149, protein: 6.4,   carbs: 33,    sugar: 1,   fiber: 2.1, fat: 0.5   },
+//     paprika: { calories: 282, protein: 14.14, carbs: 53.99, sugar: 1,   fiber: 0,   fat: 12.89 },
+//     sugar:   { calories: 387, protein: 0,     carbs: 100,   sugar: 100, fiber: 0,   fat: 0     },
+//     orange:  { calories: 49,  protein: 0.9,   carbs: 13,    sugar: 12,  fiber: 0.2, fat: 0.1   },
+//   }
+// const empty = {}
+// console.log(reduceKeys(nutritionDB, (acc, cr) => acc.concat(', ', cr)),'vinegar, sugar, oil, onion, garlic, paprika\n',)
+// console.log(reduceKeys(nutritionDB, (acc, cr) => `${acc}${cr}:`, ':'),':vinegar:sugar:oil:onion:garlic:paprika:\n',)
+// console.log(reduceKeys(nutritionDB, (acc, cr) => `${acc}${cr}:`, null),'tomato:vinegar:oil:onion:garlic:paprika:sugar:orange\n',)
