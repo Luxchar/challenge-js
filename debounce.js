796c1c264dcc41084bf1711d98834f8c63ab59ed
Luxchar <luccharlopeau@gmail.com>
2022-04-19 11:32:28 +0200

    debounce.js

--- debounce.js
+++ debounce.js
@@ -2,17 +2,23 @@
 
 //     debounce, this function doesn't need to take care of the options
 //     opDebounce, this function will take care of the leading options
-function debounce(func, wait) {
+
+function debounce(func, wait, options = {}) {
   let timeout;
   return function() {
     const context = this;
     const args = arguments;
     const later = function() {
       timeout = null;
-      func.apply(context, args);
+      if (!options.leading) {
+        func.apply(context, args);
+      }
     };
     clearTimeout(timeout);
     timeout = setTimeout(later, wait);
+    if (options.leading) {
+      func.apply(context, args);
+    }
   };
 }
 
